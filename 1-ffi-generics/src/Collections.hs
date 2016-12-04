34d4a3c11d99d1efa792c6ed233a6b302ea27e7c
Rahul Muttineni <rahulmutt@gmail.com>
2016-12-04 17:36:18 +0530

    Update example 1 for eta-0.0.3

--- 1-ffi-generics/src/Collections.hs
+++ 1-ffi-generics/src/Collections.hs
@@ -1,40 +1,20 @@
-{-# LANGUAGE MagicHash, FlexibleContexts, MultiParamTypeClasses, TypeFamilies, DataKinds #-}
+{-# LANGUAGE MagicHash, FlexibleContexts, TypeFamilies, DataKinds #-}
 module Collections where
 
 data {-# CLASS "java.util.Collection" #-} Collection a = Collection (Object# (Collection a))
+  deriving Class
 data {-# CLASS "java.util.ArrayList" #-} ArrayList a = ArrayList (Object# (ArrayList a))
+  deriving Class
 data {-# CLASS "java.util.List" #-} List a = List (Object# (List a))
+  deriving Class
 data {-# CLASS "java.lang.Integer" #-} JInteger = JInteger (Object# JInteger)
-data {-# CLASS "java.io.PrintStream" #-} PrintStream = PrintStream (Object# PrintStream)
-
-instance Class (ArrayList a) where
-  obj = ArrayList
-  unobj (ArrayList o) = o
-
-instance Class (List a) where
-  obj = List
-  unobj (List o) = o
-
-instance Class (Collection a) where
-  obj = Collection
-  unobj (Collection o) = o
-
-instance Class JInteger where
-  obj = JInteger
-  unobj (JInteger o) = o
-
-instance Class PrintStream where
-  obj = PrintStream
-  unobj (PrintStream o) = o
+  deriving Class
 
 type instance Inherits (ArrayList a) = '[List a]
 type instance Inherits (List a) = '[Collection a]
-type instance Inherits JInteger = '[Object]
 
 foreign import java unsafe "@new" newInteger :: Int -> JInteger
 foreign import java unsafe "intValue" intValue :: JInteger -> Int
-foreign import java unsafe "@new" newArrayList :: IO (ArrayList a)
+foreign import java unsafe "@new" newArrayList :: Java c (ArrayList a)
 foreign import java unsafe "add" add :: (Extends a Object, Extends b (Collection a)) => a -> Java b Bool
 foreign import java unsafe "get" get :: (Extends a Object, Extends b (List a)) => Int -> Java b a
-foreign import java unsafe "@static @field java.lang.System.out" stdout' :: PrintStream
-foreign import java unsafe "println" println :: Extends a Object => PrintStream -> a -> IO ()
--- 1-ffi-generics/src/Main.hs
+++ 1-ffi-generics/src/Main.hs
@@ -1,18 +1,19 @@
 module Main where
 
-import Java(java, io)
-import Control.Monad(forM_)
+import Java
+import Control.Monad (forM_)
 import Collections
 
 populateArray :: Int -> Java (ArrayList JInteger) ()
 populateArray n = do
-  let range = [0..n]
-  forM_ range $ \i -> add (newInteger i)
+  forM_ range $ \i ->
+    add (newInteger i)
   forM_ range $ \i -> do
     jint <- get i
     io $ print $ intValue jint * 5
+  where range = [0..n]
 
 main :: IO ()
-main = do
+main = java $ do
   list <- newArrayList
-  java list $ populateArray 10
+  list <.> populateArray 10
